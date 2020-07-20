5cf20faaa715860233d12d46800b3f30f03c3364
haomingc <haomingc1993@gmail.com>
2020-07-20 23:34:12 +0800

    rabbitmq consumer producer added

---
+++ src/test/java/com/example/rabbitmqapi/RabbitmqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
